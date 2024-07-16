903a881d56132b2a5ea2c4e258a78e606f0c108c
Leandrodeleon22 <leandrodeleon22@gmail.com>
2024-07-16 08:12:26 -0400

    last day

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import RecipePage from "@/components/RecipePage";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center">
+      <RecipePage />
+    </main>
+  );
+}
---
+++ src/components/RecipePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RecipeImage from "./RecipeImage";
+
+const RecipePage = () => {
+  return (
+    <section className="bg-white w-1/2 flex justify-center p-6 mt-20 rounded-2xl">
+      <RecipeImage />
+    </section>
+  );
+};
+
+export default RecipePage;
